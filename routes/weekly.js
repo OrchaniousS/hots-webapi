622034af60757f1d14b9dbcec136ce052728e81d
OrchaniousS <bmagramovs3@gmail.com>
2020-09-04 14:38:33 +0300

    3commit

--- routes/weekly.js
+++ routes/weekly.js
@@ -3,6 +3,14 @@ import { getWeekly } from "../controllers/weekly.js";
 
 const router = express.Router();
 
+app.all("*", function (req, res, next) {
+  var origin = req.get("origin");
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 router.get("/week", getWeekly);
 
 export default router;
